29a4d615c5ffb65e936c96910699cdd95fd1f3d9
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2022-04-21 00:43:02 +0200

    add Day2

--- index.js
+++ index.js
@@ -34,18 +34,62 @@ const game = {
             ['o', 'x', 'o'],
             ['o', 'x', 'x'],
         ],
+        rotationIndex: 0,
+        rotation: [
+            [
+                ['o', 'x', 'o'],
+                ['o', 'x', 'o'],
+                ['o', 'x', 'x'],
+            ],
+            [
+                ['o', 'o', 'x'],
+                ['x', 'x', 'x'],
+                ['o', 'o', 'o'],
+            ],
+            [
+                ['x', 'x', 'o'],
+                ['o', 'x', 'o'],
+                ['o', 'x', 'o'],
+            ],
+            [
+                ['o', 'o', 'o'],
+                ['x', 'x', 'x'],
+                ['x', 'o', 'o'],
+            ]
+        ],
     },
     moveLeft(){
-        this.activeTetramino.x -= 1;
+        if(this.checkOutPosition(this.activeTetramino.x - 1 , this.activeTetramino.y)){
+            this.activeTetramino.x -= 1;
+        }
+        
     },
     moveRight(){
-        this.activeTetramino.x += 1;
+        if(this.checkOutPosition(this.activeTetramino.x + 1, this.activeTetramino.y)){
+            this.activeTetramino.x += 1;
+        }
+        
     },
     moveDown(){
-        this.activeTetramino.y += 1;
+        if(this.checkOutPosition(this.activeTetramino.x, this.activeTetramino.y + 1)){
+            this.activeTetramino.y += 1;
+        } else {
+            this.stopMoving();
+        }
+        
     },
     rotateTetramino(){
+        this.activeTetramino.rotationIndex = 
+            this.activeTetramino.rotationIndex < 3 ? 
+                this.activeTetramino.rotationIndex + 1 : 0;
+        this.activeTetramino.block = this.activeTetramino.rotation[this.activeTetramino.rotationIndex];
 
+        if(!this.checkOutPosition(this.activeTetramino.x, this.activeTetramino.y)){
+            this.activeTetramino.rotationIndex = 
+                this.activeTetramino.rotationIndex > 0 ? 
+                    this.activeTetramino.rotationIndex - 1 : 3;
+            this.activeTetramino.block = this.activeTetramino.rotation[this.activeTetramino.rotationIndex];
+        }
     },
     get viewArea(){
         //копируем игровую область в массив
@@ -54,13 +98,39 @@ const game = {
         for (let i = 0; i < tetramino.length; i++ ){
             const row = tetramino[i];
             for (let j = 0;j < row.length; j++){
-                if(row[j] === 'x'){
+                if(row[j] !== 'o'){
                     area[y + i][x + j] = tetramino[i][j];
                 }
             }
         }
         return area;
-    }
+    },
+    checkOutPosition(x, y){
+        const tetramino = this.activeTetramino.block;
+        for (let i = 0; i < tetramino.length; i++ ){
+            
+            for (let j = 0;j < tetramino[i].length; j++){
+                if(tetramino[i][j] === 'o') continue;
+
+                if(!this.area[y + i] || !this.area[y + i][x + j] ||
+                    this.area[y + i][x + j] !== 'o'){
+                    return false;
+                }                
+            }
+        }
+        return true;
+    },
+    stopMoving(){
+        const {x, y, block: tetramino} = this.activeTetramino;
+        for (let i = 0; i < tetramino.length; i++ ){
+            const row = tetramino[i];
+            for (let j = 0;j < tetramino[i].length; j++){
+                if(row[j] !== 'o'){
+                    this.area[y + i][x + j] = tetramino[i][j];
+                }
+            }                
+        }
+    }    
 };
 
 //game.viewArea();
@@ -78,6 +148,7 @@ canvas.height = SIZE_BLOCK * 20;
 const context = canvas.getContext('2d');
 
 const showArea = (area) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     for (let y = 0; y < area.length; y++){
         const line = area[y];
 
@@ -94,5 +165,28 @@ const showArea = (area) => {
     }
     
 };
+
+window.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    const key = e.code;
+    switch(key){
+        case 'ArrowLeft':
+            game.moveLeft();
+            showArea(game.viewArea);
+            break;
+        case 'ArrowRight':
+            game.moveRight();
+            showArea(game.viewArea);
+            break;
+        case 'ArrowDown':
+            game.moveDown();
+            showArea(game.viewArea);
+            break;
+        case 'ArrowUp':
+            game.rotateTetramino();
+            showArea(game.viewArea);
+            break;
+    }
+});
 
 showArea(game.viewArea);
